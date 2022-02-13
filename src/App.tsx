03226410493fddb6aff4d539175c7a3ade5e5d0a
in-ch <sxin2949@naver.com>
2022-02-13 15:34:51 +0900

    메인 페이지 주소 변경

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,15 @@ import { BrowserRouter as Router,Switch, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Calculator from "./pages/Calculator";
 import Calculator2 from "./pages/Caluclator2";
-import Home from "./pages/Home";
 
 function App() {
   return (
     <Layout>
       <Router>
           <Switch>
-              <Route path="/" exact component={Home}>
-              </Route>
-              <Route path="/taxi" exact component={Calculator2}>
-              </Route>
-              <Route path="/bus" exact component={Calculator}>
-              </Route>
+              <Route path="/" component={Calculator} exact />
+              <Route path="/taxi" component={Calculator2} exact />
+              <Route path="/bus" component={Calculator} exact />
           </Switch>
       </Router>
     </Layout>
