a9d402aafc2e58f11ea6d4484e3948e92bf33aef
in-ch <sxin2949@naver.com>
2022-02-14 21:46:49 +0900

    hashrouter로 변경하고 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { BrowserRouter as Router,Switch, Route } from "react-router-dom";
+import { HashRouter as Router,Switch, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Calculator from "./pages/Calculator";
 import Calculator2 from "./pages/Caluclator2";
 
 function App() {
   return (
-    <Layout>
-      <Router>
+    <Router>
+        <Layout>
           <Switch>
               <Route path="/" component={Calculator} exact />
               <Route path="/taxi" component={Calculator2} exact />
               <Route path="/bus" component={Calculator} exact />
           </Switch>
-      </Router>
-    </Layout>
+        </Layout>
+    </Router>
   );
 }
 
